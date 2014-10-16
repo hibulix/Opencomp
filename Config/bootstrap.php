5c746f3d3015ea8715c3b6028800ddc05cd04798
Jean Traullé <jtraulle@gmail.com>
2014-10-16 22:47:13 +0200

    Ajout de Sentry en tant que gestionnaire d'exceptions distant

--- Config/bootstrap.php
+++ Config/bootstrap.php
@@ -150,6 +150,7 @@ Cache::config('default', array('engine' => 'File'));
  */
 CakePlugin::load('DebugKit');
 CakePlugin::load('BoostCake');
+CakePlugin::load('Sentry');
 
 /**
  * You can attach event listeners to the request lifecyle as Dispatcher Filter . By Default CakePHP bundles two filters:
