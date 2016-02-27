3bee1e893bb260af20b5fc01a16ed9dd99c0a78f
Jean Traullé <jtraulle@gmail.com>
2016-02-27 01:02:11 +0100

    Cleaning branch master

--- app/Controller/AppController.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-class AppController extends Controller {        
-    public $helpers = array('Session', 'Html', 'Form');
-	public $components = array('DebugKit.Toolbar','Session');
-        
-}
-
-?>
